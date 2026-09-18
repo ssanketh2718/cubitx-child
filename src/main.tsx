352ed4d1f30095b3c1ca0372fffc152e0a2d7d37
Sanket <ssanketh2718@gmail.com>
2026-09-18 12:24:15 +0530

    Fix build: recreate index.css, wire AuthProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './core/auth/AuthContext'
-import App from './core/App'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './core/auth/AuthContext';
+import App from './core/App';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -12,5 +12,5 @@ createRoot(document.getElementById('root')!).render(
         <App />
       </AuthProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
